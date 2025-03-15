871895c599eafe1eb74c868a5d3d4743da87bb7e
Muhmmad Saifal <saifalkhursheed@gmail.com>
2025-03-16 00:02:19 +0500

    chnage the colors code formate

---
+++ lib/utilis/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0066FF);
+  static const Color secondary = Color(0xFFFFA500);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color white = Colors.white;
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF777777);
+  static const Color buttonPrimary = Color(0xFF007BFF);
+  static const Color buttonSecondary = Color(0xFFE0E0E0);
+  static const Color border = Color(0xFFD9D9D9);
+  static const Color error = Color(0xFFFF4D4F);
+  static const Color success = Color(0xFF52C41A);
+}
