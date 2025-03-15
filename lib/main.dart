3df6ea80c464646084ccb39e065b3d344646f789
Muhmmad Saifal <saifalkhursheed@gmail.com>
2025-03-15 23:59:22 +0500

    add main class

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:get/state_manager.dart';
+import 'package:practice_get_application/screens/first_class_getx/first-class_getx.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter using Getx',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: FirstClassGetx(),
+    );
+  }
+}
